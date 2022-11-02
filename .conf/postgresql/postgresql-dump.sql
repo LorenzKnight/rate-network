ba663ff31db730f2dc1d8fc884ade852dacdbccf
Lorenz Knight <joellorenzo.k@gmail.com>
2022-11-02 15:01:35 +0100

    migrate updates

--- .conf/postgresql/postgresql-dump.sql
+++ .conf/postgresql/postgresql-dump.sql
@@ -13,24 +13,51 @@ CREATE TABLE IF NOT EXISTS users (
 );
 
 INSERT INTO users (name, surname, email, password, image, rate, signup_date, birthday, status)
-VALUES ('Shael', 'Knight', 'shael.knight@gmail.com', 123456, 'profile_pic.jpg', 0, '2022-10-18 00:00:00', '1984-09-03 00:00:00', 1),
-       ('Joel', 'Knight', 'joel.knight@gmail.com', 123456, 'profile_pic.jpg', 0, '2022-10-18 00:00:00', '1984-09-03 00:00:00', 1);
-
-ALTER TABLE users  
-ADD COLUMN rank INTEGER NULL;
+VALUES ('Lorenz', 'Knight', 'lorenz.knight@gmail.com', 123456, 'profile_pic.jpg', 0, '2022-10-18 00:00:00', '1984-09-03 00:00:00', 1),
+       ('Joel', 'Knight', 'joel.knight@gmail.com', 123456, 'profile_pic.jpg', 0, '2022-10-18 00:00:00', '1984-09-03 00:00:00', 1),
+       ('Shael', 'Knight', 'shael.knight@gmail.com', 123456, 'profile_pic.jpg', 0, '2022-10-18 00:00:00', '1984-09-03 00:00:00', 1);
 
 select * from users;
 
+ALTER TABLE users  
+ADD COLUMN image varchar(255) NULL;
 
+update users set name = 'Lorenz', image = 'profile_pic.jpg' where user_id = 1
+
+
+
+
+
+     
 CREATE TABLE IF NOT EXISTS river (
 	r_id SERIAL PRIMARY KEY,
     user_id INTEGER NULL,
-    content varchar(255) NULL
+    content varchar(255) null,
+    media_id INTEGER null,
+    status INTEGER NULL
 );
 
-INSERT INTO river (user_id, content)
-VALUES (1, 'post 1'),
-       (2, 'post 2'),
-       (3, 'post 3');
-
+INSERT INTO river (user_id, content, media_id, status)
+VALUES (1, 'post 1', 1, 1),
+       (2, 'post 2', 2, 1),
+       (3, 'post 3', 3, 1);
+       
 select * from river;
+
+
+CREATE TABLE IF NOT EXISTS rates (
+	rate_id SERIAL PRIMARY KEY,
+	stars INTEGER null,
+	rate_bonus FLOAT NULL,
+	user_id INTEGER NULL,
+	to_user_id INTEGER NULL,
+	post_id INTEGER NULL,
+	comment_id INTEGER null,
+	rate_date TIMESTAMP NULL
+);
+
+INSERT INTO rates (stars, rate_bonus, user_id, post_id, rate_date)
+VALUES (4, 0.2, 2, 3, '2022-10-18 00:00:00'),
+       (3, 0.5, 3, 3, '2022-10-18 00:00:00');
+
+select * from rates;
